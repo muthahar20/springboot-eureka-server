127388e5cd3c9fd1667b6bedcf4eb259358e1662
muthahar20@gmail.com <muthahar20@gmail.com>
2023-10-31 13:20:40 -0400

    intial commit

---
+++ src/test/java/com/mtr/springbooteurekaserver/SpringbootEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtr.springbooteurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
